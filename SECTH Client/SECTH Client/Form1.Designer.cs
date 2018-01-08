d6629529f3782b689e868762d54ad22ca8b431dc
markdebruyn <markdebruyn1992@gmail.com>
2018-01-08 08:52:41 +0100

    Change the form name

--- SECTH Client/SECTH Client/Form1.Designer.cs
+++ SECTH Client/SECTH Client/Form1.Designer.cs
@@ -153,7 +153,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Confrence Room";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
