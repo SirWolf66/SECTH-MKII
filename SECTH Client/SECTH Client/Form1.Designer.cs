99be489dfe6610f51e4e9e466f14bab3ed3ca069
markdebruyn <markdebruyn1992@gmail.com>
2018-01-17 15:10:08 +0100

    change text history bug

--- SECTH Client/SECTH Client/Form1.Designer.cs
+++ SECTH Client/SECTH Client/Form1.Designer.cs
@@ -76,6 +76,7 @@
             // 
             this.richTextBox1.Location = new System.Drawing.Point(13, 50);
             this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
             this.richTextBox1.Size = new System.Drawing.Size(468, 292);
             this.richTextBox1.TabIndex = 5;
             this.richTextBox1.Text = "";
